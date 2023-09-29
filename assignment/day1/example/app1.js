ae554a29cc4ea861629875e66c004f4b7a2518a0
Jinxu Zheng <1710198939@qq.com>
2023-09-29 16:12:33 -0500

    WAD

---
+++ assignment/day1/example/app1.js
@@ -0,0 +1,27 @@
+for (var i=1; i<=5; i++) {
+    setTimeout(function clog() {console.log(i)}, i*1000);
+    }
+/**
+ * 6
+ * 6
+ * 6
+ * 6
+ * 6
+ */
+
+    // for (let i=1; i<=5; i++) {
+    //     setTimeout(function clog() {console.log(i)}, i*1000);
+    //     }
+/**
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ */
+
+// for (var j = 1; j <= 5; j++) {
+//     var i = j;
+//     setTimeout(function clog() { console.log(i); }, i * 1000);
+//     };
+   
